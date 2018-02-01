021642c10e7dcd8f08ee97e6d980ec1d8e73d31b
Yi Zhu <yi.zhu@careeverywhere.com>
2018-02-01 11:10:53 -0500

    Separate Lexer and Parser function.

--- python/interpreter/calculator.py
+++ python/interpreter/calculator.py
@@ -4,7 +4,7 @@ class Token(object):
     def __init__(self, type, value):
         self.type = type;
         self.value = value;
-
+    
     def __str__(self):
         """String representation of class instance.
         
@@ -16,39 +16,38 @@ class Token(object):
             self.type,
             repr(self.value)
         )
-
+    
     def __repr__(self):
         return self.__str__()
 
 
-class Interpreter(object):
+class Lexer(object):
     def __init__(self, text):
-        self.text = text
+        self.text = text;
         self.pos = 0
-        self.current_token = None
         self.current_char = self.text[self.pos]
-
+    
     def error(self):
         raise Exception('Error parsing input')
-
+    
     def advance(self):
         self.pos += 1
         if self.pos > len(self.text) - 1:
             self.current_char = None
         else:
             self.current_char = self.text[self.pos]
-
+    
     def skip_whitespace(self):
         while self.current_char is not None and self.current_char.isspace():
             self.advance()
-
+    
     def integer(self):
         result = ''
         while self.current_char is not None and self.current_char.isdigit():
             result += self.current_char
             self.advance()
         return int(result)
-
+    
     def get_next_token(self):
         while self.current_char is not None:
             if self.current_char.isspace():
@@ -77,11 +76,17 @@ class Interpreter(object):
             self.error()
         return Token(EOF, None)
 
+
+class Interpreter(object):
+    def __init__(self, lexer):
+        self.lexer = lexer
+        self.current_token = None
+    
     def eat(self, token_type):
         if self.current_token.type == token_type:
-            self.current_token = self.get_next_token()
+            self.current_token = self.lexer.get_next_token()
         else:
-            self.error()
+            self.lexer.error()
 
     def term(self):
         token = self.current_token
@@ -89,7 +94,7 @@ class Interpreter(object):
         return token.value
 
     def expr(self):
-        self.current_token = self.get_next_token()
+        self.current_token = self.lexer.get_next_token()
         
         result = self.term()
         
@@ -122,7 +127,8 @@ def main():
             break
         if not text:
             continue
-        inter = Interpreter(text)
+        lexer = Lexer(text)
+        inter = Interpreter(lexer)
         result = inter.expr()
         print(result)
 
